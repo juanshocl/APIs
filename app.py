d463a91bd32a0ab492854aca170353549e4a92b2
Juan Munoz <cl.jmunoz@gmail.com>
2023-05-03 20:36:35 -0400

    generate data fron json

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
 
 app = Flask(__name__)
+with open('datos.json') as file:
+    datos = json.load(file)
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
+@app.route('/datos')
+def datos():
+    return jsonify(datos)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
